1b90e9b457b9ffc5788eb5d7d86ebd3225375d11
Reynard Mangatta Taruk <rmtaruk@gmail.com>
2022-08-14 14:51:13 +0700

    [fixing] view model lifecycle lib

--- core/src/main/java/com/rymtaruk/core/di/util/ViewModelFactory.kt
+++ core/src/main/java/com/rymtaruk/core/di/util/ViewModelFactory.kt
@@ -1,11 +1,11 @@
 package com.rymtaruk.core.di.util
 
+import javax.inject.Inject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.Factory
-import javax.inject.Inject
 import javax.inject.Provider
 
-class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, Provider<ViewModel>>): Factory {
+class ViewModelFactory @Inject constructor(private val creators: Map<Class<out ViewModel>, Provider<ViewModel>>) : Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         creators[modelClass]?.get() as T
